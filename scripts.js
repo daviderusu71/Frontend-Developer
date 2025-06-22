7b9ee96ba4eae57bc6bb6cd88e574eaf9ed2c383
daviderusu71 <140233435+daviderusu71@users.noreply.github.com>
2025-06-23 00:13:32 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  var typed = new Typed(".skills", {
+    strings: ["David Rusu", "Frontend Developer", "YouTube Creator", "UI Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1500,
+    loop: true,
+  });
+});
